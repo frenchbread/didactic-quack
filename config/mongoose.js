aca0660839fecf0e07e9d6b574f49ddafdd0a1cf
Damir Mustafin <frenchthebread@gmail.com>
2015-11-25 19:24:57 +0900

    Code cleanup. Removed mongoDB

--- config/mongoose.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var config = require('../config');
-
-var URI = config.get('ENV') == 'development' ? config.get('mongoose:uriDev') : config.get('mongoose:uriProd');
-
-mongoose.connect(URI, config.get('mongoose:options'));
-
-module.exports = mongoose;
